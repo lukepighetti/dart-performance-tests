962cd6a593cc67d3aa16c2115be12c141e9d7db0
Luke Pighetti <lukepighetti@gmail.com>
2019-01-04 17:19:13 -0500

    create/delete children

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -3,17 +3,27 @@ import 'dart:html';
 void main() {
   final e = querySelector('#output');
 
-  /// `Setting a single CSS color`
-
-  /// Test the expense of setting the style.color property
+  /// Set CSS color
   iterate("set CSS color - e..style.color – red/green", (int i) {
     e..style.color = i % 2 == 0 ? "red" : "green";
   });
 
-  /// Test the expense of setting the text property of an element
-  /// while making sure that its a new value every time
+  /// Set text content
   iterate("setText - red/green", (int i) {
     e..text = i % 2 == 0 ? "red" : "green";
+  });
+
+  /// Create sibling children
+  iterate("create children", (int i) {
+    final child = Element.div();
+    e.children.add(child);
+  });
+
+  /// Delete children
+  once("delete children", () {
+    while (e.firstChild != null) {
+      e.firstChild.remove();
+    }
   });
 }
 
@@ -32,6 +42,21 @@ iterate(String title, Function(int) f) {
   for (int i = 0; i < iterations; i++) {
     f(i);
   }
+
+  s.stop();
+
+  final time = (s.elapsedMicroseconds / 1000);
+
+  print(
+    "[dart] $title completed in ${time.toStringAsPrecision(3)}ms",
+  );
+}
+
+once(String title, Function() f) {
+  final s = Stopwatch();
+  s.start();
+
+  f();
 
   s.stop();
 
--- js/web/main.js
+++ js/web/main.js
@@ -3,17 +3,27 @@ main();
 function main() {
   var e = document.querySelector("#output");
 
-  /// `Setting a single CSS color`
-
-  /// Test the expense of setting the style.color property of an element
+  /// Set CSS color
   iterate("set CSS color - e.style.color – red/green", i => {
     e.style.color = i % 2 == 0 ? "red" : "green";
   });
 
-  /// Test the expense of setting the text property of an element
-  /// while making sure that its a new value every time
+  /// Set text content
   iterate("setText - red/green", i => {
     e.textContent = i % 2 == 0 ? "red" : "green";
+  });
+
+  /// Create sibling children
+  iterate("create children ", i => {
+    const child = document.createElement("div");
+    e.appendChild(child);
+  });
+
+  /// Delete children
+  once("delete children", i => {
+    while (e.firstChild) {
+      e.firstChild.remove();
+    }
   });
 }
 
@@ -32,6 +42,21 @@ function iterate(title, f) {
   for (var i = 0; i < iterations; i++) {
     f(i);
   }
+
+  const finish = performance.now();
+
+  var time = finish - start;
+
+  console.log(
+    `[js] ${title} completed in an average of ${time.toPrecision(3)}ms`
+  );
+}
+
+function once(title, f) {
+  //
+  const start = performance.now();
+
+  f();
 
   const finish = performance.now();
 
